49902a1c0ccc5a33fc918487760a2def0ee82a56
KreavityWorks <contact@nandomaul.online>
2026-06-29 17:15:05 +0700

    Prepare Rumah Subsidi Bogor deployment

--- src/constants.js
+++
@@ -1,8 +0,0 @@
-// --- PT AKG DF PERSONNEL DATA ---
-export const salesTeam = [
-    { id: 'u3', name: 'Tasya', roles: ['TikTok Official Partner', 'Official Store', 'Akaso'] },
-    { id: 'u4', name: 'Cecep', roles: ['TikTok Official Partner', 'Official Store', 'KOL'] },
-    { id: 'u2', name: 'Defrin', roles: ['Official Store Akulaku', 'Live Stream Akaso'] },
-    { id: 'u6', name: 'Rizka', roles: ['Official Store Tokopedia', 'iDreamtech'] },
-    { id: 'u7', name: 'Hadist', roles: ['iDreamtech', 'Akaso'] }
-];
