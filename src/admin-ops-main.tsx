92fdb85d47041df02bc5c95f747e315414d9d5a4
Nando Maulana <projects@kreavityworks.com>
2026-08-31 15:31:49 +0700

    Add admin operations preview entry

---
+++ src/admin-ops-main.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import AdminOperations from './admin/operations/AdminOperations';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <AdminOperations />
+  </React.StrictMode>
+);
